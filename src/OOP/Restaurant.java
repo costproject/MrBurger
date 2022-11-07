5f2a3617b83b3022e63747df739c4da8872b6b1c
CRISTEEN <CRISTEEN@LAPTOP-ULBDR2SL>
2022-11-07 11:08:56 +0530

    yo yo added

--- src/OOP/Restaurant.java
+++ src/OOP/Restaurant.java
@@ -16,6 +16,7 @@ public class Restaurant {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello");
+        System.out.println("yo yo");
     }
     
 }
